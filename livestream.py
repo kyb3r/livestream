d825b364ef8d05d3e671f2b2be284d0a028a18a4
kyb3r <abdurraqeeb53@gmail.com>
2019-07-14 13:55:56 +1000

    Use proper list

--- livestream.py
+++ livestream.py
@@ -2,21 +2,23 @@ import os
 import random
 import subprocess
 
-STREAM_URL = os.getenv('STREAM_URL')
+STREAM_URL = os.getenv("STREAM_URL")
+
 
 def livestream_files():
-    directory = os.listdir('videos')
+    directory = os.listdir("videos")
     random.shuffle(directory)
     for video in directory:
-        command = (
-            f"ffmpeg -re -i 'videos/{video}' -c:v libx264 -preset veryfast -maxrate 3000k "
-            f"-bufsize 6000k -pix_fmt yuv420p -g 50 -c:a aac -b:a 160k -ac 2 "
-            f"-ar 44100 -f flv {STREAM_URL}"
-        ).split()
+        command = [
+            "ffmpeg", "-re", "-i", f"videos/{video}", "-c:v", "libx264",
+            "-preset", "veryfast", "-maxrate", "3000k", "-bufsize", "6000k",
+            "-pix_fmt", "yuv420p", "-g", "50", "-c:a", "aac", "-b:a", "160k",
+            "-ac", "2", "-ar", "44100", "-f", "flv", STREAM_URL,
+        ]
         print("Starting livestream for:", video)
         subprocess.run(command, capture_output=True)
         try:
-            os.remove('None')
+            os.remove("None")
         except FileNotFoundError:
             pass
         print("Finished livestream for:", video)
@@ -27,5 +29,5 @@ while True:
     try:
         livestream_files()
     except KeyboardInterrupt:
-        print('\nStopped livestream')
+        print("\nStopped livestream")
         break
